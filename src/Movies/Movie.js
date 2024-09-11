027ad2a29ce7d99bc7728da70d77f5157d614e37
Sarah Pickering <sarahpsellsaz@gmail.com>
2024-09-11 16:06:47 -0700

    wrapped app with BrowserRouter. imported routes, movielist, movie, and savedlist. Used declarative and imperative approach to navigate to specific movie. Used params hook to read URL in Movie.js

--- src/Movies/Movie.js
+++ src/Movies/Movie.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom'
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
+  const { id } = useParams()
 
   let id = 1;
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
